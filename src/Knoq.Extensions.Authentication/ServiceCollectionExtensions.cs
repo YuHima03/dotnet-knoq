81ef04e2797163cb66bce65c7fa5443adf551f64
YuHima03 <yuhima03@gmail.com>
2025-04-18 18:38:06 +0900

    ✨ DI コンテナ内で自動で knoQ の認証できるように

---
+++ src/Knoq.Extensions.Authentication/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Traq;
+
+namespace Knoq.Extensions.Authentication
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAuthenticatedKnoqApiClient(this IServiceCollection services, Action<KnoqApiClientOptions> configureKnoq, TraqAuthenticationInfo traqAuthInfo)
+        {
+            return services
+                .AddOptions()
+                .AddSingleton<IConfigureOptions<KnoqApiClientOptions>>(sp => {
+                    return new ConfigureNamedOptions<KnoqApiClientOptions>(Options.DefaultName, options => {
+                        configureKnoq.Invoke(options);
+                        var traqOptions = sp.GetRequiredService<IOptions<TraqApiClientOptions>>().Value;
+                        var task = AuthenticationExtensions.GetKnoqAccessTokenAsync(options.BaseAddress, traqOptions.BaseAddress, traqAuthInfo, CancellationToken.None).AsTask();
+                        task.Wait();
+                        options.CookieAuthToken = task.Result;
+                    });
+                })
+                .AddSingleton<IKnoqApiClient, KnoqApiClient>();
+        }
+    }
+}

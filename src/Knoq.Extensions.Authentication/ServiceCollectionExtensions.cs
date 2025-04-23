43e7b4b3e681dac95601eeb643779f5e5c9ba9a9
YuHima03 <yuhima03@gmail.com>
2025-04-23 16:01:07 +0900

    ✨ TraqAuthenticationInfo の設定方法を変更

--- src/Knoq.Extensions.Authentication/ServiceCollectionExtensions.cs
+++ src/Knoq.Extensions.Authentication/ServiceCollectionExtensions.cs
@@ -6,14 +6,21 @@ namespace Knoq.Extensions.Authentication
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddAuthenticatedKnoqApiClient(this IServiceCollection services, Action<KnoqApiClientOptions> configureKnoq, TraqAuthenticationInfo traqAuthInfo)
+        public static IServiceCollection AddAuthenticatedKnoqApiClient(this IServiceCollection services, Action<KnoqApiClientOptions> configureKnoq, Action<IServiceProvider, TraqAuthenticationInfo> configureTraqAuth)
         {
             return services
                 .AddOptions()
                 .AddSingleton<IConfigureOptions<KnoqApiClientOptions>>(sp => {
-                    return new ConfigureNamedOptions<KnoqApiClientOptions>(Options.DefaultName, options => {
-                        configureKnoq.Invoke(options);
+                    return new ConfigureNamedOptions<KnoqApiClientOptions>(Options.DefaultName, options =>
+                    {
+                        // options
                         var traqOptions = sp.GetRequiredService<IOptions<TraqApiClientOptions>>().Value;
+                        TraqAuthenticationInfo traqAuthInfo = new();
+
+                        // configure
+                        configureKnoq.Invoke(options);
+                        configureTraqAuth.Invoke(sp, traqAuthInfo);
+
                         var task = AuthenticationExtensions.GetKnoqAccessTokenAsync(options.BaseAddress, traqOptions.BaseAddress, traqAuthInfo, CancellationToken.None).AsTask();
                         task.Wait();
                         options.CookieAuthToken = task.Result;
